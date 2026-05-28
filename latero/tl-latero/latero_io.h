4b7ffc65a3322f0411aa1e92eb32484549cf725c
Vincent Levesque <vincent@vlevesque.com>
2026-05-28 15:16:59 +0300

    Switching from LGPL to MIT license.

--- latero/tl-latero/latero_io.h
+++ latero/tl-latero/latero_io.h
@@ -1,25 +1,3 @@
-// -----------------------------------------------------------
-//
-// Copyright (c) 2015 by Vincent Levesque. All Rights Reserved.
-//
-// This file is part of latero.
-//
-//    latero is free software: you can redistribute it and/or modify
-//    it under the terms of the GNU Lesser General Public License as published by
-//    the Free Software Foundation, either version 3 of the License, or
-//    (at your option) any later version.
-//
-//    latero is distributed in the hope that it will be useful,
-//    but WITHOUT ANY WARRANTY; without even the implied warranty of
-//    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-//    GNU Lesser General Public License for more details.
-//
-//    You should have received a copy of the GNU Lesser General Public License
-//    along with latero.  If not, see <http://www.gnu.org/licenses/>.
-//
-// -----------------------------------------------------------
-
-
 #pragma once
 
 #ifdef __cplusplus
